fa151a8aa2756670ca7c4f12088086f077bc0cc6
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-11-03 15:13:09 +0000

    add api_constraints class to pass tests

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,11 +5,11 @@ class ApiConstraints
   end
 
   def matches?(request)
-    @version || request[:headers]['Accept'].include?("application/vnd.marketplace.v#{@version}")
+    @default || request[:headers]['Accept'].include?("application/vnd.marketplace.v#{@version}")
   end
 end
 
-const_1 = ApiConstraints.new(version: 1)
-const_2 = ApiConstraints.new(version: 2, default: true)
-request = { host: 'api.marketplace.dev' }
-print const_1.matches?(request)
+# const_1 = ApiConstraints.new(version: 1)
+# const_2 = ApiConstraints.new(version: 2, default: true)
+# request = { host: 'api.marketplace.dev', headers: { "Accept" => 'application/vnd.marketplace.v1' }}
+# print const_1.matches?(request)
--- lib/spec/api_constraints_spec.rb
+++ lib/spec/api_constraints_spec.rb
@@ -8,7 +8,7 @@ describe ApiConstraints do
   describe "matches?" do
     it "returns true when the version matches the 'Accept' header" do
       request = { host: 'api.marketplace.dev',
-                  headers: { "Accept" => 'applictaion/vnd.marketplace.v1' }
+                  headers: { "Accept" => 'application/vnd.marketplace.v1' }
                 }
       expect(api_constraints_v1.matches?(request)).to eq(true)
     end
